0c75289a5432b49d71f783f4f98a824001f4adf3
Luis Augusto <lsm.liborio@gmail.com>
2026-03-06 03:39:51 -0300

    commit before init dvc

--- mycode.py
+++ mycode.py
@@ -6,3 +6,12 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'City': ['New York', 'Aracaju', 'Chicago']}
 
 df = pd.DataFrame(data)
+
+data_dir = 'data'
+
+os.makedirs(data_dir, exist_ok=True)
+
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+df.to_csv(file_path, index=False)
+print(f"CSV file saved to {file_path}")
