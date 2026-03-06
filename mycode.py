ec6f7cc7b8ec690cfd432af746fea89a67a0db6c
Luis Augusto <lsm.liborio@gmail.com>
2026-03-06 03:31:35 -0300

    first commit

---
+++ mycode.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 34],
+        'City': ['New York', 'Aracaju', 'Chicago']}
+
+df = pd.DataFrame(data)
